a50791c0a18a15851a677118432ddd9cbe16f7b6
zitiso <zitiso@gmail.com>
2024-07-07 14:33:31 -0700

    Add project to local repo

---
+++ app/src/main/java/com/example/materialdesign/starwars/MovieView.kt
@@ -0,0 +1,25 @@
+package com.webage.materialdesignsolution.starwars
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun MovieView(){
+
+
+}
+    
---
+++ app/src/main/java/com/example/materialdesign/starwars/MovieViewModel.kt
@@ -0,0 +1,29 @@
+package com.webage.materialdesignsolution.starwars
+
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+import kotlinx.coroutines.launch
+
+class MovieViewModel : ViewModel() {
+
+    private val _movieWrapper : MutableState<MovieWrapper> =
+        mutableStateOf(MovieWrapper(0,null,null,emptyList<Movie>()))
+
+    val movieWrapper get() = _movieWrapper
+
+    fun getMovies() {
+        viewModelScope.launch {
+            val movieService = MovieService.getInstance()
+            try {
+                _movieWrapper.value = movieService.getMovies()
+            } catch (e: Exception) {
+                Log.wtf("GET_MOVIES",e.message)
+            }
+        }
+    }
+
+}
